e3d84c792c20149202dd1c6d4ed410f317d7e83b
nick155042 <56065917+nick155042@users.noreply.github.com>
2020-06-05 10:36:23 +0800

    Add files via upload

---
+++ 太空穿梭/太空穿梭py.py
@@ -0,0 +1,17 @@
+import pygame
+
+# 初始化 pyGame 引擎
+pygame.init()
+# 設定 pyGame 遊戲視窗 * 標題 *
+pygame.display.set_caption("Hello pygame!")
+# 設定 pyGame 遊戲視窗 * 大小 *
+win = pygame.display.set_mode((640, 480))
+game_over = False
+# 遊戲主迴圈
+while not game_over:
+    for event in pygame.event.get():
+        if (event.type == pygame.QUIT):
+            game_over = True
+# 將遊戲視窗繪製 (貼) 到螢幕上
+    pygame.display.flip()
+pygame.quit()
