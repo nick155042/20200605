97cf822c73e98bf616f8e252082118b601ef4cc7
nick155042 <56065917+nick155042@users.noreply.github.com>
2020-06-06 12:01:04 +0800

    Update 太空穿梭py.py

--- 太空穿梭/太空穿梭py.py
+++ 太空穿梭/太空穿梭py.py
@@ -1,19 +1,26 @@
+import random
 import pygame
 
-# 初始化
-pygame.init()
+clock=pygame.time.Clock()
+
 # 設置視窗標題為 太空穿梭:)
 pygame.display.set_caption('太空穿梭:)') 
 # 建立 window 視窗畫布，大小為 640x480
-window = pygame.display.set_mode((640, 480))
-game_over = False
-bg=pygame.image.load("bg.png")
+win = pygame.display.set_mode((640, 480))
+BG=pygame.image.load("bg.png")
 HJ= pygame.image.load("hj.png")
-while True:
+UN=pygame.image.load("un.png")
+pygame.display.set_icon(UN)
+
+
+
+game_over = False
+
+
+while not game_over:
     for event in pygame.event.get():
         # 當使用者結束視窗，程式也結束
         if (event.type ==  pygame. QUIT):
             game_over=True
     pygame.display.flip()
 pygame.quit()
-
