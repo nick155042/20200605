ca67752f9f125aa23d0f3f33e8d09fd386a30b05
nick155042 <56065917+nick155042@users.noreply.github.com>
2020-06-06 12:11:39 +0800

    Update 太空穿梭py.py

--- 太空穿梭/太空穿梭py.py
+++ 太空穿梭/太空穿梭py.py
@@ -3,14 +3,14 @@ import pygame
 
 clock=pygame.time.Clock()
 
-# 設置視窗標題為 太空穿梭:)
-pygame.display.set_caption('太空穿梭:)') 
-# 建立 window 視窗畫布，大小為 640x480
-win = pygame.display.set_mode((640, 480))
+screen = pygame.display.set_mode((500, 400))
 BG=pygame.image.load("bg.png")
+screen.blit(BG,(0,0))
+pygame.display.update
+pygame.display.set_caption('太空穿梭:)') 
 HJ= pygame.image.load("hj.png")
 UN=pygame.image.load("un.png")
-pygame.display.set_icon(UN)
+pygame.display.set_icon(HJ)
 
 
 
